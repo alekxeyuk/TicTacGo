f121b0aed9b7ef9d9e0668ae7016303a33f2a5b9
LeLuCh B0й <9300326+alekxeyuk@users.noreply.github.com>
2022-08-09 03:22:23 +0700

    start

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import "github.com/gin-gonic/gin"
+
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	return r
+}
+
 func main() {
 	println("Hello, world!")
+	r := setupRouter()
+	r.Run(":8080")
 }
