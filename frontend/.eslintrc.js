003946ee91ceeed97ecd0022c7bd0bfe0f0dc499
LeLuCh B0й <9300326+alekxeyuk@users.noreply.github.com>
2022-08-21 02:52:12 +0700

    shit does not work

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,4 +1,4 @@
-{
+module.exports = {
     "env": {
         "browser": true,
         "es2021": true,
@@ -11,17 +11,13 @@
     ],
     "overrides": [
         {
-            // Define the configuration for `.astro` file.
-            "files": ["*.astro"],
-            // Allows Astro components to be parsed.
-            "parser": "astro-eslint-parser",
-            // Parse the script in `.astro` as TypeScript by adding the following configuration.
-            // It's the setting you need when using TypeScript.
-            "parserOptions": {
-                "parser": "@typescript-eslint/parser",
-                "extraFileExtensions": [".astro"],
+            files: ["*.astro"],
+            parser: "astro-eslint-parser",
+            parserOptions: {
+                parser: "@typescript-eslint/parser",
+                extraFileExtensions: [".astro"],
             },
-            "rules": {
+            rules: {
                 // override/add rules settings here, such as:
                 // "astro/no-set-html-directive": "error"
             },
