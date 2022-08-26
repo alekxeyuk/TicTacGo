c818addac0fdfbd122f21e51ebbe13e41d170e44
LeLuCh B0й <9300326+alekxeyuk@users.noreply.github.com>
2022-08-26 22:06:37 +0700

    Receive events from SSE store

--- frontend/src/components/Counter.svelte
+++ frontend/src/components/Counter.svelte
@@ -1,18 +1,27 @@
-<script>
+<script type="ts">
     import { onMount } from "svelte";
+    import { createChannelStore } from "../stores/indexChannelStore";
 
-    
+
 	let count = 0;
 
     // get the rooms count from the server
     async function getRoomsCount() {
-        const response = await fetch('/api/rooms');
+        const response = await fetch('http://localhost:80/room/count');
         const data = await response.json();
-        return data.count;
+        count = data.count;
     }
 
-    onMount(async () => {
-        count = await getRoomsCount();
+    onMount(() => {
+        getRoomsCount();
+
+        const store = createChannelStore('global');
+
+        store.subscribe(incomingMessages => {
+            console.log(incomingMessages);
+        });
+
+        return store.close;
     });
 </script>
 
