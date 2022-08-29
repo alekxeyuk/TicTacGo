d873fa3bf37aad05da4c467fa5289d891b10ac04
LeLuCh B0й <9300326+alekxeyuk@users.noreply.github.com>
2022-08-29 17:33:56 +0700

    router changes

--- backend/main.go
+++ backend/main.go
@@ -25,36 +25,31 @@ func setupRouter(router *gin.Engine) {
 	router.Use(UserMiddleware())
 	router.GET("/", index)
 	router.GET("/ping", ping)
-	// router.GET("/random", random)
 
 	rooms := router.Group("/room")
 	{
-		rooms.POST("/new", roomNEW)
+		rooms.GET("/random", roomRANDOM)
+		rooms.GET("/:roomid/stream", roomSTREAM)
 		rooms.GET("/count", roomCOUNT)
-		rooms.GET("/list", roomLIST)
-		rooms.DELETE("/:roomid", roomDELETE)
-	}
-
-	sse := router.Group("/stream")
-	{
-		sse.GET("/:roomid", roomSTREAM)
+		// rooms.GET("/list", roomLIST)
+		// rooms.DELETE("/:roomid", roomDELETE)
 	}
 }
 
 func CORSMiddleware() gin.HandlerFunc {
-    return func(c *gin.Context) {
-        c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-        c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-        c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
 
-        if c.Request.Method == "OPTIONS" {
-            c.AbortWithStatus(204)
-            return
-        }
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
 
-        c.Next()
-    }
+		c.Next()
+	}
 }
 
 func StartGin() {
