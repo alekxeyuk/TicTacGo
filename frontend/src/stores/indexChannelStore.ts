2bccda33eb221b70fbb35dfe8a482526116eb0ee
LeLuCh B0й <9300326+alekxeyuk@users.noreply.github.com>
2022-08-29 17:32:56 +0700

    update SSE store

--- frontend/src/stores/indexChannelStore.ts
+++ frontend/src/stores/indexChannelStore.ts
@@ -1,10 +1,11 @@
 import { writable } from "svelte/store";
 
-export const createChannelStore = (channelId: string, eventName: string) => {
+export const createChannelStore = (channelId: string, eventName: string, withAuth: boolean) => {
     const { subscribe, set } = writable('');
 
     const eventSource = new EventSource(
-        `http://localhost:80/stream/${channelId}`
+        `http://localhost:80/room/${channelId}/stream`,
+        { withCredentials: withAuth }
     );
 
     eventSource.addEventListener(eventName, (event) => {
