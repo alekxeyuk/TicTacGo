9cdef390463e3c749c942851423ab963834d218c
LeLuCh B0й <9300326+alekxeyuk@users.noreply.github.com>
2022-09-04 15:52:52 +0700

    Game complete

--- backend/rooms.go
+++ backend/rooms.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"sync"
+	"time"
 
 	"github.com/dustin/go-broadcast"
 	"github.com/google/uuid"
@@ -34,9 +35,20 @@ type Room struct {
 	b             broadcast.Broadcaster
 	board         [9]PlayerSign
 	boardLock     sync.RWMutex
-	currentPlayer PlayerSign
+	currentPlayer *User
 	users         [2]*User
 	state         RoomState
+}
+
+var winCheckLines = [8][3]int64{
+	{0, 1, 2},
+	{3, 4, 5},
+	{6, 7, 8},
+	{0, 3, 6},
+	{1, 4, 7},
+	{2, 5, 8},
+	{0, 4, 8},
+	{2, 4, 6},
 }
 
 // Creating a map of room IDs to broadcasters.
@@ -53,7 +65,7 @@ func openListener(roomid string) chan interface{} {
 
 // It closes the listener channel and unregisters it from the room.
 func closeListener(roomid string, listener chan interface{}) {
-	if room := getRoom(roomid); room.id != "" {
+	if room := getRoom(roomid); room != nil {
 		room.b.Unregister(listener)
 	}
 	close(listener)
@@ -73,8 +85,11 @@ func (r *Room) addUser(user *User) {
 	r.boardLock.Lock()
 	defer r.boardLock.Unlock()
 	if r.users[0] == nil {
+		user.sign = PLAYER_X
+		r.currentPlayer = user
 		r.users[0] = user
 	} else {
+		user.sign = PLAYER_O
 		r.users[1] = user
 	}
 }
@@ -84,19 +99,22 @@ func (r *Room) addUser(user *User) {
 func newRoom() (r *Room, b broadcast.Broadcaster) {
 	roomid := uuid.NewString()
 	b = broadcast.NewBroadcaster(10)
-	r = &Room{roomid, b, [9]PlayerSign{}, sync.RWMutex{}, PLAYER_X, [2]*User{}, EMPTY_ROOM}
+	r = &Room{roomid, b, [9]PlayerSign{}, sync.RWMutex{}, nil, [2]*User{}, EMPTY_ROOM}
 	rooms[roomid] = r
 	roomsCounter++
 	return
 }
 
-func deleteRoom(roomid string) bool {
+func deleteRoom(roomid string, mess interface{}) bool {
 	r, ok := rooms[roomid]
 	if ok {
-		r.b.Submit(Message{"stop", ""})
-		r.b.Close()
-		delete(rooms, roomid)
-		roomsCounter--
+		r.b.Submit(mess)
+		go func() {
+			time.Sleep(1 * time.Second)
+			r.b.Close()
+			delete(rooms, roomid)
+			roomsCounter--
+		}()
 	}
 	return ok
 }
@@ -107,29 +125,35 @@ func getRoom(roomid string) *Room {
 	return rooms[roomid]
 }
 
-// func (r *Room) getBoard() [9]PlayerSign {
-// 	r.boardLock.RLock()
-// 	defer r.boardLock.RUnlock()
-// 	return r.board
-// }
-
-// func (r *Room) setBoard(board [9]PlayerSign) {
-// 	r.boardLock.Lock()
-// 	defer r.boardLock.Unlock()
-// 	r.board = board
-// }
-
 func (r *Room) move(index int64) {
 	r.boardLock.Lock()
 	defer r.boardLock.Unlock()
-	r.board[index] = r.currentPlayer
+	r.board[index] = r.currentPlayer.sign
 	r.switchPlayer()
 }
 
 func (r *Room) switchPlayer() {
-	if r.currentPlayer == PLAYER_X {
-		r.currentPlayer = PLAYER_O
+	if r.currentPlayer.sign == PLAYER_X {
+		r.currentPlayer = r.users[1]
 	} else {
-		r.currentPlayer = PLAYER_X
+		r.currentPlayer = r.users[0]
 	}
+}
+
+func (r *Room) cellIsEmpty(index int64) bool {
+	r.boardLock.RLock()
+	defer r.boardLock.RUnlock()
+	return r.board[index] == EMPTY_CELL
+}
+
+func (r *Room) checkWin() bool {
+	r.boardLock.Lock()
+	defer r.boardLock.Unlock()
+	for _, line := range winCheckLines {
+		a, b, c := line[0], line[1], line[2]
+		if r.board[a] == r.board[b] && r.board[b] == r.board[c] && r.board[a] != EMPTY_CELL {
+			return true
+		}
+	}
+	return false
 }
